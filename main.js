70f64c8db5f539835a244f789a2ee5beb0798a4c
MaltbyRL <RLMaltby@gmail.com>
2016-03-19 16:08:11 -0700

    collision det active

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
     var screen = canvas.getContext('2d');
     var gameSize = { x: canvas.width, y: canvas.height };
 
-    this.bodies = [new Player(this, gameSize)]
+    this.bodies = createInvaders(this).concat(new Player(this, gameSize))
 
     var self = this;
     var tick = function() {
@@ -18,6 +18,18 @@
 
   Game.prototype = {
     update: function() {
+
+
+      var bodies = this.bodies;
+      var notCollidingWithAnything = function(b1) {
+          return bodies.filter(function(b2) {
+            return colliding(b1, b2);
+          }).length === 0;
+      }
+
+      this.bodies = this.bodies.filter(notCollidingWithAnything);
+
+
       for (var i = 0; i < this.bodies.length; i ++){
         this.bodies[i].update();
       }
@@ -61,13 +73,28 @@ var Invader = function(game, center) {
   this.size = {x:15, y: 15};
   this.center = center;
   this.patrolX = 0;
-  this.speedX = 0.5;
+  this.speedX = 2;
 }
 
 Invader.prototype = {
   update: function() {
+    if (this.patrolX < 0 || this.patrolX > 500){
+      this.speedX = -this.speedX
+    }
 
+    this.center.x += this.speedX;
+    this.patrolX += this.speedX;
   }
+};
+
+var createInvaders = function(game) {
+  var invaders = [];
+  for (var i = 0; i < 24; i++){
+    var x = 30 + (i % 8) * 30;
+    var y = 30 + (i % 3) * 30;
+    invaders.push(new Invader(game, { x: x, y: y}))
+  }
+  return invaders;
 }
 
 
@@ -81,7 +108,7 @@ var Bullet = function(center, velocity) {
 Bullet.prototype = {
   update: function() {
       this.center.x += this.velocity.x;
-      this.center.y += this.velocity.y * 1.5;
+      this.center.y += this.velocity.y / 1.5;
 
 
   }
@@ -98,15 +125,20 @@ var Keyboarder = function() {
   };
 
   this.isDown = function(keyCode) {
+
     return keyState[keyCode] === true;
   };
 
   this.KEYS = { LEFT: 37, RIGHT: 39, SPACE: 32}
-
-
-
 };
 
+var colliding = function(b1, b2) {
+  return !(b1 === b2 ||
+          b1.center.x + b1.size.x/2 < b2.center.x - b2.size.x / 2 ||
+          b1.center.y + b1.size.y/2 < b2.center.y - b2.size.y / 2 ||
+          b1.center.x - b1.size.x/2 > b2.center.x + b2.size.x / 2 ||
+          b1.center.y - b1.size.y/2 > b2.center.y + b2.size.y / 2);
+}
 var drawRect = function(screen, body) {
   screen.fillRect(body.center.x - body.size.x/2,
                   body.center.y - body.size.y/2,
